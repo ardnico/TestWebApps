bb4a0ee70fa689332bc7c4ef7488ad40b051e76f
ardnico <leaf.sun2@gmail.com>
2021-09-03 21:55:19 +0900

    rewrite

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World"
+# @app.route('/')
+# def hello():
+#     return "Hello World"
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8888, threaded=True)  
+# if __name__ == "__main__":
+#     app.run(debug=True, port=8888, threaded=True)
+print("HelloWorld")
